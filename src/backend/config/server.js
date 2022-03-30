bc5decc63c2e610b3af36dfd179642e2f4fff593
Braden Cariaga <11431488+Syzco@users.noreply.github.com>
2022-03-30 16:42:58 -0400

    Refactor and add docs

---
+++ src/backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1672e6e7e6d473d909276d635f2bb079'),
+    },
+  },
+});
